26f4de9845d9e5f40b5d3ade39f2910de72f2445
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-09-02 21:36:35 +0530

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,60 @@
+// import { IconHexagonLetterK } from "@tabler/icons-react";
+import SideBar from "./SideBar";
+import { useMediaQuery } from "@mantine/hooks";
+import { em } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+const links=["About","Projects","Skills","Experience","Contact"];
+const navLinks=(col:Boolean, clicked:any)=>{
+    const handleClick=()=>{
+        if(clicked)clicked();
+    }
+    return links.map((link, index)=>{
+        return  <a key={index} onClick={handleClick} className={`${col?'flex flex-col items-center':''} text-textColor text-lg font-mono hover:text-primaryColor`} href={`#${link}`}><span className="text-primaryColor"> </span>{link}</a>
+    })
+}
+
+const IconHexagonLetterPortfolio = () => (
+    <div className="flex justify-center items-center">
+        <div className="relative w-24 h-14 bg-green-400">
+            <div className="absolute -top-3.5 w-0 h-0 border-l-12 border-r-12 border-b-[14px] border-l-transparent border-r-transparent border-b-green-400"></div>
+            <div className="absolute -bottom-3.5 w-0 h-0 border-l-12 border-r-12 border-t-[14px] border-l-transparent border-r-transparent border-t-green-400"></div>
+            <span className="absolute inset-0 flex items-center justify-center text-white font-bold text-sm">
+                Portfolio
+            </span>
+        </div>
+    </div>
+);
+
+
+
+
+const Header=()=>{
+    const isMobile = useMediaQuery(`(max-width: ${em(476)})`);
+    const [show, setShow] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [shadow, setShadow] = useState(false);
+    const controlNavbar = () => {
+        if(window.scrollY>lastScrollY && window.scrollY>70)setShow(false);
+        else setShow(true);
+        if(window.scrollY>70)setShadow(true);
+        else setShadow(false);
+        setLastScrollY(window.scrollY);
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', controlNavbar);
+        return ()=>window.removeEventListener('scroll', controlNavbar);
+    })
+return (
+    <nav className={`flex ${show?"translate-y-0":"-translate-y-28"} ${shadow?"shadow-[0px_10px_30px_-10px_#020c1b]":""} transition-transform duration-500 ease-in-out fixed w-full z-10 bg-bgColor h-28  px-10  justify-between items-center xs-mx:px-4 xs-mx:h-20 `}>
+        
+        <IconHexagonLetterPortfolio />
+        <div className="bs:flex gap-8 hidden">
+            {navLinks(false, null)}
+        </div>
+        <SideBar/>
+    </nav>
+);
+}
+export default Header;
+export {navLinks};
---
+++ src/User.tsx
@@ -0,0 +1,150 @@
+import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube , IconCode, IconTerminal} from "@tabler/icons-react";
+const Info = {
+    name: "Kuldeep",
+    stack: ["Software Engineer", "Full Stack Developer"],
+    bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
+}
+
+
+
+const ProjectInfo = [
+    {
+        title: "BookShoppe E-comm App",
+        desc: "BookShoppe is a modern, fully responsive e-commerce platform built using React, Vite, TailwindCSS, Redux, and MaterialUI, offering a seamless shopping experience across different book categories. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a cart or remove them, search for specific products and purchase them. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. BookShoppe combines robust functionality, intuitive design, making online Book shopping effortless and enjoyable.",
+        image: "BookShoppe.png",
+        live: true,
+        technologies: ["MERN", "Vite", "Tailwind", "Redux"],
+        link: "https://book-shoppe.vercel.app/",
+        github: "https://github.com/kuldeep1441/BookShoppe.git"
+    },
+    {
+        title: "ApnaGPT",
+        desc: "ApnaGPT is an advanced MERN stack application integrated with OpenAI's API to deliver intelligent features like text summarization, chatbot interactions, JavaScript code generation, and text-to-image conversion. The platform supports secure user authentication, ensuring privacy and data protection. Users can interact with a responsive chatbot, generate sci-fi images, or receive JavaScript code suggestions. The application showcases the powerful combination of AI and web development, providing users with a dynamic and engaging experience..",
+        image: "ApnaGPT.png",
+        live: false,
+        technologies: ["MERN", "OpenAI", "Bootstrap", "Redux"],
+        link: "https://github.com/kuldeep1441/ApnaGPT.git",
+        github: "https://github.com/kuldeep1441/ApnaGPT.git"
+    },
+    {
+        title: "Portfolio",
+        desc: "The Portfolio Website is a personal project developed using Typescript, React, and Tailwind, and is deployed on Vercel. It features a clean, intuitive design with sections dedicated to About, Home, and Projects, allowing easy navigation. The Contact Us section is integrated to enable visitor interaction, with MongoDB handling the efficient storage and management of visitor details. This portfolio highlights technical skills, project experiences, and provides a seamless way for potential employers and collaborators to connect.",
+        image: "Portfolio.png",
+        live: false,
+        technologies: ["React", "Typescript", "Tailwind"],
+        link: "https://github.com/Code-Mars/Spotify-Clone",
+        github: "https://github.com/Code-Mars/Spotify-Clone"
+    },
+    // {
+    //     title: "Travel Tracker",
+    //     desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
+    //     image: "Travel.png",
+    //     live: false,
+    //     technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
+    //     link: "https://github.com/Code-Mars/Travel-Tracker",
+    //     github: "https://github.com/Code-Mars/Travel-Tracker"
+    // },
+    // {
+    //     title: "Instagram Clone",
+    //     desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
+    //     image: "Instagram.png",
+    //     live: false,
+    //     technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
+    //     link: "https://github.com/Code-Mars/Instagram-Clone",
+    //     github: "https://github.com/Code-Mars/Instagram-Clone"
+    // },
+    // {
+    //     title: "CodeX Code Editor",
+    //     desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
+    //     image: "CodeX.png",
+    //     live: false,
+    //     technologies: ["React", "Tailwind", "Ace Editor"],
+    //     link: "https://github.com/Code-Mars/CodeX",
+    //     github: "https://github.com/Code-Mars/CodeX"
+    // }
+]
+
+
+const SkillInfo = [
+    {
+        title: "Frontend",
+        skills: ["HTML", "CSS", "JavaScript", "React JS", "Next JS", "Redux", "Tailwind CSS", "Typescript", "Material UI", "Bootstrap"
+        ]
+    },
+    {
+        title: "Backend",
+        skills: [ "Node JS", "Express JS", "MongoDB", "MySQL"]
+    },
+    {
+        title: "Languages",
+        skills: ["C", "C++", "HTML", "Sql", "Python", "JavaScript", "TypeScript"]
+    },
+    {
+        title: "Tools",
+        skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass", "Vite"]
+    }
+]
+const socialLinks = [
+    { link: "https://github.com/kuldeep1441", icon: IconBrandGithub },
+    { link: "https://www.linkedin.com/in/kuldeep-tanwar-61b748237/", icon: IconBrandLinkedin },
+    { link: "https://leetcode.com/u/kuldeep_kd/", icon: IconBrandLeetcode },
+    { link: "https://www.geeksforgeeks.org/user/kuldeep1441/", icon: IconCode }, 
+    { link: "https://www.interviewbit.com/profile/kuldeep_kd/", icon: IconTerminal }
+];
+
+
+const ExperienceInfo = [
+    {
+        role: "Software Developer Intern",
+        company: "Vilihi Virtual Services",
+        date: "January 2024 - Present",
+        desc: "I worked on the development of Mult-Vendor E-commerce website using  MERN, Redux, and Tailwind, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in teams, I fostered innovation and efficiency for top-quality solutions.",
+        skills: ["MongoDB", "Express", "React JS", "Node JS", "Redux", "Tailwind Css"]
+    },
+    // {
+    //     role: "System Engineer",
+    //     company: "Infosys",
+    //     date: "Nov 2022 - Sep 2023",
+    //     desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
+    //     skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
+    // }
+]
+const Slugs = [
+    "typescript",
+    "spring",
+    "javascript",
+    "dart",
+    "java",
+    "react",
+    "angular",
+    "flutter",
+    "android",
+    "html5",
+    "css3",
+    "springboot",
+    "mongodb",
+    "selenium",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "prisma",
+    "mysql",
+    "amazonaws",
+    "postgresql",
+    "firebase",
+    "nginx",
+    "vercel",
+    "testinglibrary",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "gitlab",
+    "visualstudiocode",
+    "androidstudio",
+    "sonarqube",
+    "figma",
+];
+export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
